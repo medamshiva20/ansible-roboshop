1655aa7136c341cd899e62485ba3122f116eddaa
medamshiva20 <medamshiva20@gmail.com>
2023-12-29 23:32:10 +0530

    create servers through shell script

--- create-servers.sh
+++ create-servers.sh
@@ -8,11 +8,11 @@ SECURITY_GROUP_ID=sg-001887f7841106c09
 
 for i in $@
 do
- if [[ $i == 'mongodb' || $i == 'mysql' ]]
+ if [[ $i == "mongodb" || $i == "mysql" ]]
   then
      INSTANCE_TYPE="t3.medium"
   else
-     INSTANCE_TYPE="t2.medium"
+     INSTANCE_TYPE="t2.micro"
  fi
  echo creating $i instance
  aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID 
